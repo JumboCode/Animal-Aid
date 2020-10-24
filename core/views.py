cf13d32052767004d3366716c71a2604c042e007
Sejal Dua <sejal.dua@tufts.edu>
2020-10-24 10:04:21 -0700

    form validation stuff- still need tufts.edu validation

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def home(request):
     return render(request, 'core/home.html')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -11,11 +13,15 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
+            if not username.endswith('@cowhite.com'):
+                return render(request, 'core/signup.html', {'form': form})
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            # return redirect('home')
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'core/signup.html', {'form': form})
     
+def error(request):
+    return render(request, 'core/error.html')
