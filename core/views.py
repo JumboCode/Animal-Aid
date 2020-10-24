b8597d6abbb2e2b1a8b57084cf024ed3d65deec4
Sejal Dua <sejal.dua@tufts.edu>
2020-10-24 15:03:16 -0700

    fixed tufts email validation

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+import django.contrib.auth.forms as forms
 
 def home(request):
     return render(request, 'core/home.html')
@@ -10,10 +11,15 @@ def home(request):
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        print(form.as_p())
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            if not username.endswith('@cowhite.com'):
+            try:
+                form.save()
+                username = form.cleaned_data.get('username')
+                if not username.endswith('@tufts.edu'):
+                    form.add_error("username", "Please enter a valid Tufts email address")
+                    raise forms.ValidationError("Please enter a valid Tufts email address")
+            except:
                 return render(request, 'core/signup.html', {'form': form})
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
