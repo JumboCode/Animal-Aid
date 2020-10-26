4a4c139a2979c11786137d49383488148d2259cd
Tyler <tyler.thompson@tufts.edu>
2020-10-26 00:00:55 -0400

    progress

--- AnimalAid/urls.py
+++ AnimalAid/urls.py
@@ -22,10 +22,10 @@ from core import views as core_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', core_views.home, name="home"),
-    path('password-reset/', core_views.pass_reset, name="password-reset"),
     path('', core_views.home, name="home"),
-    #path("password_reset", views.password_reset_request, name="password_reset"),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'), 
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password/password_reset.html'), name='password_reset'),
+    #path('password_reset/', core_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name='password_reset_complete'), 
 ]
