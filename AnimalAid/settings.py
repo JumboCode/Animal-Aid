c5e5d1e571769fe643d2489330b685841959eb70
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2020-11-04 12:21:25 -0500

    added admin css and js

--- AnimalAid/settings.py
+++ AnimalAid/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -136,4 +138,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '../static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
