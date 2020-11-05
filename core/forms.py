a2b1a1b8341841ea7bbe6c0b1cdb83eacfabd353
Sejal Dua <sejal.dua@tufts.edu>
2020-11-04 22:49:06 -0800

    working dynamic validation functionality yayyy

--- core/forms.py
+++ core/forms.py
@@ -6,6 +6,9 @@ from django.core.exceptions import ValidationError
 from django.http import JsonResponse
 from django.contrib.auth.password_validation import *
 
+
+SpecialSym =['$', '@', '#', '%', '*', '!', '^', '&', '(', ')', '?']
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(label='Enter Tufts email')
     password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
@@ -38,24 +41,51 @@ class CustomUserCreationForm(UserCreationForm):
         )
         return user
 
+
+################ DYNAMIC SIGNUP VALIDATION FUNCS #######################
+
+# make sure the email ends with tufts.edu
+# we may want to make sure it is unique later (but not doing that dynamically)
 def validate_username(request):
     email = str(request.GET.get('email',None))
     data = {
         'tufts_edu': email.endswith("@tufts.edu"),
     }
-    if (data['tufts_edu'] == True):
-        print("yay")
     return JsonResponse(data)
 
-def validate_password1(request):
-    SpecialSym =['$', '@', '#', '%', '*']
-    password1 = str(request.GET.get('password1',None))
+# check if the password meets the 4 following criteria:
+    # over 6 characters long
+    # contains a special character (not weak)
+    # not a common tester password that is easy to hack
+    # is not only numeric
+def check_password(passwd):
     data = {
-        'too_short': len(password1) < 6,
-        'no_special_char': not any(char in SpecialSym for char in password1),
-        'too_common': password1 == "test" or password1 == "testtest" or "123" in password1 or "abc" in password1,
-        'only_digits': password1.isdigit()
+        'too_short': len(passwd) < 6,
+        'no_special_char': not any(char in SpecialSym for char in passwd),
+        'too_common': passwd == "test" or passwd == "testtest" or "123" in passwd or "abc" in passwd,
+        'only_digits': passwd.isdigit()
     }
+    return data
+
+# helper function to ensure that the password meets all 4 criteria
+def synthesize(data):
+    return True if all([not value for value in data.values()]) else False
+
+# validate first password field dynamically
+def validate_password1(request):
+    password1 = str(request.GET.get('password1',None))
+    data = check_password(password1)
+    return JsonResponse(data)
+
+# validate second password field dynamically
+# this time, also check to ensure that the two passwords match
+def validate_password2(request):
+    password1 = str(request.GET.get('password1',None))
+    password2 = str(request.GET.get('password2',None))
+    pwd1_data = check_password(password1)
+    data = dict()
+    data['strong_pass'] = synthesize(pwd1_data)
+    data['is_matching'] = password1 == password2
     return JsonResponse(data)
 
 class LoginForm(AuthenticationForm):
