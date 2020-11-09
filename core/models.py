1444e6f4eb85dad87d59d9f3b7fbee4c618bb513
Janny Huang <janny.huang@tufts.edu>
2020-11-09 16:02:17 -0500

    rename model for dog

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Dog(models.Model):
+class DogModel(models.Model):
     name = models.CharField(max_length=30)
     location = models.CharField(max_length=100)
     
