3aa8ba8464ceb4b2dc7f48013a8ee6e3e73d0b18
larrythexu <lawrencexu8@gmail.com>
2020-11-14 19:34:59 -0500

    added some more fields to the model

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Form(models.Model):
 	form_name = models.CharField(max_length=100)
@@ -9,10 +10,31 @@ class Form(models.Model):
 
 
 class Field(models.Model):
+	#make Field a child of Form
 	forms = models.ForeignKey(Form, on_delete=models.CASCADE, default=1)
-	# textField = models.CharField(max_length=200)
-	# votes = models.IntegerField(default=0)
 
-	# forms = models.ForeignKey(Form, on_delete=models.CASCADE)
-	textField = models.CharField(max_length=200)
-	testBool = models.BooleanField('test bool', default=1)
+	#setting up label name
+	label = models.CharField(max_length=200)
+
+	#determining which field type
+	#defining constants
+	TEXT_FIELD = 'TF'
+	CHECKLIST = 'CL'
+	NUMBER = 'NM'
+	DROPDOWN = "DD"
+
+	FIELD_TYPES = [
+		('TF', 'Text Field'),
+		('CL', 'Checklist'),
+		('NM', 'Number'),
+		('DD', 'Dropdown Menu'),
+	]
+
+	formType = models.CharField(max_length=20, choices=FIELD_TYPES, default='')
+
+	#required and visible bools
+	requiredBool = models.BooleanField('field required', default=1)
+	visibleBool = models.BooleanField('field visible', default=1)
+
+	#ordering - validator to make sure it stays positive (applies only when made into ModelForm)
+	order = models.IntegerField('field order', null=True, blank=True, validators=[MinValueValidator(0)])
