c95a7bc39fc34e5070ee5b5be399a7ffac9bdabb
Emily <emilynadler12@gmail.com>
2020-12-02 15:55:28 -0500

    added each time option for each dog (very cluttered but trying to figure out how to make some sort of table), and added table for dog display to give more info for each dog

--- core/admin.py
+++ core/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Dog, Walker
 
-admin.site.register(Dog)
+class Dogs(admin.ModelAdmin):
+    list_display= ('name', 'location', 'zip_code')
+  
+admin.site.register(Dog, Dogs)
+
 admin.site.register(Walker)
