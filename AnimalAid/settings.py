00b4704278dcacd97d293625c5d3b14955c032e2
vietnguyen00 <viet.nguyen@tufts.edu>
2021-01-19 21:32:31 -0500

    Add allowed hosts

--- AnimalAid/settings.py
+++ AnimalAid/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://hidden-taiga-39776.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
