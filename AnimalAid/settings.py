59190f7b52926984d364bda772a0e763fb7024fd
vietnguyen00 <viet.nguyen@tufts.edu>
2021-01-29 04:00:59 -0500

    No core data error

--- AnimalAid/settings.py
+++ AnimalAid/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os, time, sys
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,4 @@ STATICFILES_DIRS = (
 )
 
 LOGOUT_REDIRECT_URL = '/'
+django_heroku.settings(locals())
