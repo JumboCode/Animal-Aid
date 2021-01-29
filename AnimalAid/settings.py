5b2a3afab7dceec79bdc753fc3035318d03aee24
vietnguyen00 <viet.nguyen@tufts.edu>
2021-01-29 02:41:14 -0500

    Fix  Heroku set up

--- AnimalAid/settings.py
+++ AnimalAid/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["animalaidtufts.herokuapp.com", "0.0.0.0"]
 
 
 # Application definition
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
