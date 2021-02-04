42ff1f7175ac65b965c0f23a711f713e604522c3
larrythexu <lawrencexu8@gmail.com>
2021-02-03 21:15:58 -0500

    adjusting admin form, not actual dog model

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,21 @@ from .models import Dog, Walker
 
 class Dogs(admin.ModelAdmin):
     list_display= ('name', 'location', 'zip_code')
+
+
+class DogModelForm(forms.modelForm):
+	class Meta:
+		model = Dog
+		fields = '__all__'
+
+class DogModelAdmin(admin.ModelAdmin)
+	form = DogModelForm
+
+	fieldsets = (
+		(None, {
+			'fields': ('dog_name', 'info', 'owner_name', 'address', 'image', 'walk_times')
+			}),
+		)
   
 admin.site.register(Dog, Dogs)
 
