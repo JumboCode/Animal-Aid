27c42f7518d8896901e099fa35b1b26800af6d7b
larrythexu <lawrencexu8@gmail.com>
2021-02-03 21:20:23 -0500

    updating extra fields

--- core/admin.py
+++ core/admin.py
@@ -8,6 +8,13 @@ class Dogs(admin.ModelAdmin):
 
 
 class DogModelForm(forms.modelForm):
+	#additional fields
+	info = models.CharField(max_length=200)
+    owner_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    #MISSING: WALK TIMES
+
+
 	class Meta:
 		model = Dog
 		fields = '__all__'
