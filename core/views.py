f1d2422b856c31f1873ed7edd630c0fe918cf256
Tyler <tyler.thompson@tufts.edu>
2021-02-14 14:58:15 -0500

    Added dog list and edit dog page for staff. Added profile page for users to modify their personal info, times they're available and prefered dogs

--- core/views.py
+++ core/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth import login as auth_login, authenticate
 from django.core.exceptions import ValidationError
 from .models import Dog
 from django.core.exceptions import PermissionDenied, EmptyResultSet
-from core.models import Dog
+from core.models import Dog, Walker
+from json import dumps
 
 def home(request):
     return render(request, 'core/home.html')
@@ -47,10 +48,11 @@ def dog_gallery(request):
     dogs = Dog.objects.all()
     dog_infos = []
     for dog in dogs:
-        dog_info = {}
-        dog_info["name"] = dog.name
-        dog_info["image_path"] = dog.image.url 
-        dog_infos.append(dog_info)
+        if dog.get_visible():
+            dog_info = {}
+            dog_info["name"] = dog.name
+            dog_info["image_path"] = ''#dog.image.url 
+            dog_infos.append(dog_info)
     
     return render(request, 'core/dog.html', {'dogs': dog_infos})
     
@@ -110,26 +112,206 @@ def results(request):
     else:
         raise PermissionDenied()
 
-def edit_dog(request):
+def dog_list(request):
+    # only able to view master list if logged in as staff
     if request.user.is_authenticated and request.user.is_staff:
+        # get all dogs in db
+        dogs = Dog.objects.all()
+        data = {'dogs':[]}
+        # for each dog populate a dictionary w/ name, img, location, and db id
+        for dog in dogs:
+            data['dogs'].append({
+                'name': dog.get_name,
+                'image': dog.get_image,
+                'location': dog.get_location,
+                'id': dog.id,
+            })
+        return render(request, 'core/dog_list.html', data) 
+    else:
+        raise PermissionDenied()
+
+# constants to change walking days and times
+# Sizes should match DAYS and HOURS constants in core/models.py
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+HOURS = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']
+
+def edit_dog(request):
+    # only able to edit dogs if logged in as staff
+    if request.user.is_authenticated and request.user.is_staff:
+
+        # get dog with id matchin=g url parameter
+        dog_name = request.GET.get('q', '')
+
+        # redirect back to list if id doesn't exist in db
+        if not Dog.objects.filter(id=dog_name).exists():
+            return redirect('dog_list')
+        selected_dog = Dog.objects.get(id=dog_name)
+
         # POST request
         if request.method == 'POST':
-            return redirect('home')
+
+            # save dog info if save button is pressed
+            if 'save_dog' in request.POST:
+                # assigning name, location, and zip from POST request
+                selected_dog.name = request.POST.get('dog_name')
+                selected_dog.location = request.POST.get('dog_location')
+                selected_dog.zip_code = request.POST.get('dog_zip')
+                selected_dog.visible = request.POST.get('dog_visible') == 'on'
+
+                # assigning times from POST using checkbox id convention: "Thursday-2:00pm"
+                chosen_times = []
+                for day in DAYS:
+                    for hour in HOURS:
+                        chosen_times.append(request.POST.get(day + '-' + hour) == 'on')
+                selected_dog.times = chosen_times
+
+                selected_dog.save()
+
+            # delete dog entry if delete button is pressed
+            elif 'delete' in request.POST:
+                selected_dog.delete()
+
+            return redirect('dog_list')
         else:
-            dog_name = request.GET.get('q', '')
-            if not Dog.objects.filter(name=dog_name).exists():
-                return redirect('results')
-            selected_dog = Dog.objects.get(name=dog_name)
+            # two dictionaries passed to render:
+            #   data: used by django framework to format walk times table
+            #   json_data: used by js to display all stored walk times
             data = {
                 'dog': {
                     'name': selected_dog.get_name,
                     'image': selected_dog.get_image,
                     'location': selected_dog.get_location,
-                    'times': selected_dog.get_walktimes,
+                    'zip': selected_dog.get_zip,
                 },
-                'days': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-                'hours': ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm'],
+                'days': DAYS,
+                'hours': HOURS,
             }
-            return render(request, 'core/edit_dog.html', data)
+            json_data = {
+                'days': DAYS,
+                'hours': HOURS,
+                'visible': selected_dog.get_visible(),
+                'times': selected_dog.get_walktimes(),
+            }
+            return render(request, 'core/edit_dog.html', {'data':data, 'json_data':dumps(json_data)})
+    else:
+        # permission denied if user isn't staff
+        raise PermissionDenied()
+
+def add_dog(request):
+    # only able to add dogs if logged in as staff
+    if request.user.is_authenticated and request.user.is_staff:
+
+        # POST request
+        if request.method == 'POST':
+
+            if 'save_dog' in request.POST:
+                # assigning name, location, and zip from POST request
+                name_in = request.POST.get('dog_name')
+                location_in = request.POST.get('dog_location')
+                zip_code_in = request.POST.get('dog_zip')
+                visible_in = request.POST.get('dog_visible') == 'on'
+
+                # assigning times from POST using checkbox id convention: "Thursday-2:00pm"
+                chosen_times = []
+                for day in DAYS:
+                    for hour in HOURS:
+                        chosen_times.append(request.POST.get(day + '-' + hour) == 'on')
+                
+                new_dog = Dog(
+                    name=name_in,
+                    location=location_in,
+                    zip_code=zip_code_in,
+                    visible=visible_in,
+                    times=chosen_times,
+                    image='',
+                )
+
+                new_dog.save()
+            
+            return redirect('dog_list')
+        else:
+            data = {
+                'days': DAYS,
+                'hours': HOURS,
+            }
+            return render(request, 'core/add_dog.html', data)
+    else:
+        # permission denied if user isn't staff
+        raise PermissionDenied()
+
+# TODO staff needs to be able to change this number
+PREF_COUNT = 5
+
+def edit_walker(request):
+    # only able to edit walker profile if logged in as a normal user, not staff
+    if request.user.is_authenticated and not request.user.is_staff:
+        
+        username = request.user.get_username()
+
+        # gets walker db entry if it already exists
+        if Walker.objects.filter(email=username).exists():
+            walker = Walker.objects.get(email=username)
+        
+        # creates a matching walker entry if one doesn't already exist
+        else:
+            walker = Walker(
+                name='',
+                email=username,
+            )
+            walker.save()
+
+        name = walker.get_name()
+        email = walker.get_email()
+        phone_number = walker.get_phone_number()
+        if phone_number == None:
+            phone_number = ''
+
+        # POST request saves data to walker model
+        if request.method == 'POST':
+            walker.name = request.POST.get('walker_name')
+            walker.phone_number = request.POST.get('walker_phone')
+            chosen_times = []
+            for day in DAYS:
+                for hour in HOURS:
+                    chosen_times.append(request.POST.get(day + '-' + hour) == 'on')
+            walker.times = chosen_times
+
+            walker.dog_choices = []
+            for choice_num in range(PREF_COUNT):
+                dropdown_choice = request.POST.get(f'dog_select_{choice_num + 1}')
+                if Dog.objects.filter(name=dropdown_choice).exists():
+                    walker.dog_choices.append(Dog.objects.get(name=dropdown_choice).get_name())
+                else:
+                    walker.dog_choices.append(None)
+
+            walker.save()
+
+        all_dogs = Dog.objects.all()
+        visible_dog_names = []
+        for dog in all_dogs:
+            if dog.get_visible():
+                visible_dog_names.append(dog.get_name())
+        
+        pref_nums = range(1, PREF_COUNT+1)
+
+        data = {
+            'walker': {
+                'name': name,   
+                'email': email,
+                'phone': phone_number,
+            },
+            'days': DAYS,
+            'hours': HOURS,
+            'saved': (request.method == 'POST'),
+            'dog_names': visible_dog_names,
+            'pref_nums': pref_nums,
+        }
+        json_data = {
+            'days': DAYS,
+            'hours': HOURS,
+            'times': walker.get_walktimes(),
+            'dog_choices': walker.get_dog_choices(),
+        }
+        return render(request, 'core/edit_walker.html', {'data':data, 'json_data':dumps(json_data)})
     else:
         raise PermissionDenied()
