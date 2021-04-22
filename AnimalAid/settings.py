bb94103f2a7e336e203ebf26765502268a918eb6
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2021-04-21 23:50:30 -0400

    added thumbnail area

--- AnimalAid/settings.py
+++ AnimalAid/settings.py
@@ -178,7 +178,7 @@ STATICFILES_DIRS = (
 )
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 LOGOUT_REDIRECT_URL = '/'
 # https://stackoverflow.com/questions/61111988/django-keeps-using-wrong-storage-backend-when-trying-to-upload-static-files-to-s
