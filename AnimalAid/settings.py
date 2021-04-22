a403448fa03858282c2b018c6bc7445bb457ae0e
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2021-04-22 00:20:09 -0400

    updating one setting

--- AnimalAid/settings.py
+++ AnimalAid/settings.py
@@ -177,7 +177,7 @@ STATICFILES_DIRS = (
     (os.path.join(BASE_DIR, 'static/')),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
