dc13ec7e6b9a4e7b65ddd92b561b0bc3d37fa49e
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2021-04-22 00:10:57 -0400

    more image fixes

--- AnimalAid/settings.py
+++ AnimalAid/settings.py
@@ -176,6 +176,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     (os.path.join(BASE_DIR, 'static/')),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
