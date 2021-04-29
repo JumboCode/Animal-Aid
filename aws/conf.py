ef27b66e57a65a9ef16289df544ce0d5b1a38650
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2021-04-28 20:34:00 -0400

    removed comments with outdated data

--- aws/conf.py
+++ aws/conf.py
@@ -4,14 +4,6 @@ env = os.environ
 
 
 # All values set in heroku configs
-# AWS_STORAGE_BUCKET_NAME = 'animal-aid-local'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_FILE_OVERWRITE = True
-# AWS_ACCESS_KEY_ID = "***REMOVED***"
-# AWS_SECRET_ACCESS_KEY = "***REMOVED***"
-# AWS_S3_REGION_NAME = "us-east-2"
-# AWS_S3_ENDPOINT_URL = 'https://s3.us-east-2.amazonaws.com'
-
 AWS_STORAGE_BUCKET_NAME = env['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_CUSTOM_DOMAIN = env['AWS_S3_CUSTOM_DOMAIN']
 AWS_S3_FILE_OVERWRITE = bool(int(env['AWS_S3_FILE_OVERWRITE']))
